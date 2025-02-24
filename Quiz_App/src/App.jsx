ea2d5210d4546a63b822bc6ee32d95359d41427f
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-02-24 23:34:17 +0530

    this is Quizapp

--- Quiz_App/src/App.jsx
+++ Quiz_App/src/App.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1 className='p-10 text-blue-400 text-5xl'>this is a quiz app</h1>
+    <div className='p-10'>
+      <h1 className='p-4 flex justify-center text-5xl text-amber-50 font-bold'>Here you can test your knowledge</h1>
+      <div className='text-3xl text-amber-50  mt-2'>
+        question</div>
+      <div className='text-2xl text-amber-50 mt-2'>
+        option</div>
+      <div className='text-2xl text-amber-50  mt-2'>
+        option</div>
+      <div className='text-2xl text-amber-50  mt-2'>
+        option</div>
+      <div className='text-2xl text-amber-50  mt-2'>
+        option</div>
+      <div >
+        <button className='text-3xl bg-green-400 rounded mt-5 text-black p-2 delay-700 hover:bg-amber-300 cursor-pointer '> Next </button>
+      </div>
+     
     </div>
   )
 }
